20a71a20db696ee07450d5db41e5a03495537066
tainakarorue <tainakarorue@gmail.com>
2021-08-10 10:20:53 +0900

    mutate Cart created

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -1,5 +1,6 @@
 import { useAddItem } from '@common/cart';
 import { UseAddItem } from '@common/cart/use-add-item';
+import useCart from '@common/cart/use-cart';
 import { Cart } from '@common/types/cart';
 import { MutationHook } from '@common/types/hooks';
 import { CheckoutLineItemsAddPayload } from '@framework/schema';
@@ -45,8 +46,11 @@ export const handler: MutationHook<AddItemHookDescriptor> = {
   useHook:
     ({ fetch }) =>
     () => {
+      const { mutate: updateCart } = useCart();
+
       return async (input) => {
         const response = await fetch(input);
+        await updateCart(response, false);
         return response;
       };
     },
